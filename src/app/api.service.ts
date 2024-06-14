24b3eb9a3878cd7deb538cbf137388c0ee127e23
Wrajt <kgrabarek59@gmail.com>
2024-06-14 14:34:33 +0200

    added: sorting cards by mana cost, displaying all cards available in the game

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { CLIENT_ID, CLIENT_SECRET } from "./API_KEYS.const";
-import { Observable } from 'rxjs';
-import { tap, switchMap } from 'rxjs/operators';
+import { CLIENT_ID, CLIENT_SECRET } from './API_KEYS.const';
+import { Observable, of } from 'rxjs';
+import { tap, switchMap, catchError } from 'rxjs/operators';
 import { CardDataInterface, Card } from './interfaces/card-data.interface';
 
 @Injectable({
@@ -14,10 +14,11 @@ export class ApiService {
   private accessToken: string | null = null;
 
   constructor(private http: HttpClient) {
-    console.log("API Service is working!");
+    console.log('API Service is working!');
   }
+
   private getToken(): Observable<any> {
-    console.log("Fetching access token...");
+    console.log('Fetching access token...');
     const headers = new HttpHeaders({
       'Content-Type': 'application/x-www-form-urlencoded',
       'Authorization': 'Basic ' + btoa(CLIENT_ID + ':' + CLIENT_SECRET)
@@ -27,20 +28,22 @@ export class ApiService {
     return this.http.post(this.tokenUrl, body.toString(), { headers: headers })
       .pipe(
         tap((response: any) => {
-          console.log("Token response:", response);
+          console.log('Token response:', response);
           this.accessToken = response.access_token;
-          console.log("Access token set:", this.accessToken);
+          console.log('Access token set:', this.accessToken);
         })
       );
   }
+
   private fetchCardBacks(): Observable<any> {
     if (!this.accessToken) {
-      throw new Error("Access token is null");
+      throw new Error('Access token is null');
     }
     const url = `https://us.api.blizzard.com/hearthstone/cardbacks?sort=dateAdded%3Adesc&access_token=${this.accessToken}`;
-    console.log("Fetching card backs with token:", this.accessToken);
+    console.log('Fetching card backs with token:', this.accessToken);
     return this.http.get(url);
   }
+
   getCardBacks(): Observable<any> {
     if (this.accessToken) {
       return this.fetchCardBacks();
@@ -51,21 +54,47 @@ export class ApiService {
     }
   }
 
-private fetchAllCards(): Observable<CardDataInterface>{
-    if(!this.accessToken){
-      throw new Error("Access token is null");
+  private fetchPage(page: number, pageSize: number = 500): Observable<CardDataInterface> {
+    if (!this.accessToken) {
+      throw new Error('Access token is null');
     }
-    const url = `https://us.api.blizzard.com/hearthstone/cards?locale=en_US&pageSize=9028&access_token=${this.accessToken}`;
-    console.log("Fetching all cards with token:", this.accessToken);
-    return this.http.get<CardDataInterface>(url)
-}
-getAllCards(): Observable<CardDataInterface>{
-    if (this.accessToken){
+    const url = `https://us.api.blizzard.com/hearthstone/cards?locale=en_US&pageSize=${pageSize}&page=${page}&access_token=${this.accessToken}`;
+    console.log(`Fetching page ${page} with token:`, this.accessToken);
+    return this.http.get<CardDataInterface>(url);
+  }
+
+  private fetchAllCards(): Observable<Card[]> {
+    let allCards: Card[] = [];
+    let page = 1;
+
+    const fetchNextPage = (): Observable<Card[]> => {
+      return this.fetchPage(page).pipe(
+        switchMap(data => {
+          allCards = [...allCards, ...data.cards];
+          if (data.cards.length === 500) { // Check if this is the last page
+            page++;
+            return fetchNextPage();
+          } else {
+            return of(allCards);
+          }
+        }),
+        catchError(error => {
+          console.error('Error fetching cards:', error);
+          return of(allCards);
+        })
+      );
+    };
+
+    return fetchNextPage();
+  }
+
+  getAllCards(): Observable<Card[]> {
+    if (this.accessToken) {
       return this.fetchAllCards();
     } else {
       return this.getToken().pipe(
         switchMap(() => this.fetchAllCards())
       );
     }
-}
+  }
 }
--- src/app/card-info/card-info.component.ts
+++ src/app/card-info/card-info.component.ts
@@ -1,50 +1,53 @@
-import {Component, Input, OnInit} from '@angular/core';
-import { ApiService} from "../api.service";
+import { Component, Input, OnInit } from '@angular/core';
+import { ApiService } from "../api.service";
 import { CommonModule, NgForOf } from '@angular/common';
-import {MatTab, MatTabGroup, MatTabsModule} from '@angular/material/tabs';
+import { MatTab, MatTabGroup, MatTabsModule } from '@angular/material/tabs';
 import { Card } from '../interfaces/card-data.interface';
 
 @Component({
   selector: 'app-card-info',
   standalone: true,
-  imports: [NgForOf, CommonModule, MatTabGroup, MatTab,MatTabsModule],
+  imports: [NgForOf, CommonModule, MatTabGroup, MatTab, MatTabsModule],
   templateUrl: './card-info.component.html',
-  styleUrl: './card-info.component.css'
+  styleUrls: ['./card-info.component.css']
 })
-export class CardInfoComponent implements OnInit{
-  @Input() label: string = ''
+export class CardInfoComponent implements OnInit {
+  @Input() label: string = '';
 
-  allCards: Card[] = []
-  warriorCards: Card[] = []
-  druidCards: Card[] = []
-  hunterCards: Card[] = []
-  mageCards: Card[] = []
-  paladinCards: Card[] = []
-  priestCards: Card[] = []
-  rogueCards: Card[] = []
-  shamanCards: Card[] = []
-  warlockCards: Card[] = []
-  neutralCards: Card[] = []
+  allCards: Card[] = [];
+  warriorCards: Card[] = [];
+  druidCards: Card[] = [];
+  hunterCards: Card[] = [];
+  mageCards: Card[] = [];
+  paladinCards: Card[] = [];
+  priestCards: Card[] = [];
+  rogueCards: Card[] = [];
+  shamanCards: Card[] = [];
+  warlockCards: Card[] = [];
+  neutralCards: Card[] = [];
+
+  constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
-    this.apiService.getAllCards().subscribe((data: { cards: Card[] }) => {
-      this.allCards = data.cards;
-      this.filterCards();
-      console.log("Warrior Cards" , this.warriorCards);
+    this.apiService.getAllCards().subscribe((cards: Card[]) => {
+      console.log('Total cards fetched:', cards.length);
+      this.allCards = cards;
+      this.filterAndSortCards();
+      console.log('Warrior Cards:', this.warriorCards);
     });
   }
 
-  filterCards(): void {
-    this.warriorCards = this.getUniqueCards(this.allCards.filter(card => card.classId === 10));
-    this.druidCards = this.getUniqueCards(this.allCards.filter(card => card.classId === 2));
-    this.hunterCards = this.getUniqueCards(this.allCards.filter(card => card.classId === 3));
-    this.mageCards = this.getUniqueCards(this.allCards.filter(card => card.classId === 4));
-    this.paladinCards = this.getUniqueCards(this.allCards.filter(card => card.classId === 5));
-    this.priestCards = this.getUniqueCards(this.allCards.filter(card => card.classId === 6));
-    this.rogueCards = this.getUniqueCards(this.allCards.filter(card => card.classId === 7));
-    this.shamanCards = this.getUniqueCards(this.allCards.filter(card => card.classId === 8));
-    this.warlockCards = this.getUniqueCards(this.allCards.filter(card => card.classId === 9));
-    this.neutralCards = this.getUniqueCards(this.allCards.filter(card => card.classId === 12));
+  filterAndSortCards(): void {
+    this.warriorCards = this.sortByManaCost(this.getUniqueCards(this.allCards.filter(card => card.classId === 10)));
+    this.druidCards = this.sortByManaCost(this.getUniqueCards(this.allCards.filter(card => card.classId === 2)));
+    this.hunterCards = this.sortByManaCost(this.getUniqueCards(this.allCards.filter(card => card.classId === 3)));
+    this.mageCards = this.sortByManaCost(this.getUniqueCards(this.allCards.filter(card => card.classId === 4)));
+    this.paladinCards = this.sortByManaCost(this.getUniqueCards(this.allCards.filter(card => card.classId === 5)));
+    this.priestCards = this.sortByManaCost(this.getUniqueCards(this.allCards.filter(card => card.classId === 6)));
+    this.rogueCards = this.sortByManaCost(this.getUniqueCards(this.allCards.filter(card => card.classId === 7)));
+    this.shamanCards = this.sortByManaCost(this.getUniqueCards(this.allCards.filter(card => card.classId === 8)));
+    this.warlockCards = this.sortByManaCost(this.getUniqueCards(this.allCards.filter(card => card.classId === 9)));
+    this.neutralCards = this.sortByManaCost(this.getUniqueCards(this.allCards.filter(card => card.classId === 12)));
   }
 
   getUniqueCards(cards: Card[]): Card[] {
@@ -57,5 +60,8 @@ export class CardInfoComponent implements OnInit{
       return false;
     });
   }
-  constructor(private apiService: ApiService) {}
+
+  sortByManaCost(cards: Card[]): Card[] {
+    return cards.sort((a, b) => a.manaCost - b.manaCost);
+  }
 }
