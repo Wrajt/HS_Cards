487dc77084added724e82fbe9a795483493c983a
Wrajt <kgrabarek59@gmail.com>
2024-07-07 10:17:03 +0200

    deleted keys

--- src/app/API_KEYS.const.ts
+++ src/app/API_KEYS.const.ts
@@ -1,2 +1,2 @@
-export const CLIENT_ID = '40b762b5f1344991bbfc43d5c4e8b56a';
-export const CLIENT_SECRET = 'bYiTn6UDjUIaXXXjA2rxW3Juo2B9JxnE';
+export const CLIENT_ID = '';
+export const CLIENT_SECRET = '';
