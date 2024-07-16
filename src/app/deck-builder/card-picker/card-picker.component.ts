93abc0fb01a2d60cfcf0c73859ce0003aa582d0a
Wrajt <kgrabarek59@gmail.com>
2024-07-16 09:31:17 +0200

    card-picker wip

--- src/app/deck-builder/card-picker/card-picker.component.ts
+++ src/app/deck-builder/card-picker/card-picker.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import {NgForOf, TitleCasePipe} from "@angular/common";
+import {Component, OnInit} from '@angular/core';
+import {NgForOf, NgIf, TitleCasePipe} from "@angular/common";
+import {ApiService} from "../../api.service";
+import {PaginatorComponent} from "../../paginator/paginator.component";
 
 @Component({
   selector: 'app-card-picker',
@@ -7,18 +9,44 @@ import {NgForOf, TitleCasePipe} from "@angular/common";
   standalone: true,
   imports: [
     NgForOf,
-    TitleCasePipe
+    TitleCasePipe,
+    NgIf,
+    PaginatorComponent
   ],
   styleUrls: ['./card-picker.component.css']
 })
-export class CardPickerComponent {
-  classNames = ['warrior', 'mage', 'druid', 'hunter', 'paladin', 'priest', 'rogue', 'shaman', 'warlock'];
+export class CardPickerComponent implements OnInit {
+  classNames = ['warrior', 'mage', 'druid', 'hunter', 'paladin', 'priest', 'rogue', 'shaman', 'warlock', 'neutral'];
+  selectedClass: string = '';
+  classCards: any[] = [];
+  cards: any[] = [];
+  filteredCards: any[] = [];
 
-  constructor() {}
 
-  onClassChange(event: Event) {
-    const selectedClass = (event.target as HTMLSelectElement).value;
-    console.log('Selected class:', selectedClass);
+  constructor(private apiService: ApiService) {}
 
+  ngOnInit(): void {}
+
+  onClassChange(event: Event): void {
+    this.selectedClass = (event.target as HTMLSelectElement).value;
+    this.loadCards();
+  }
+
+  loadCards(): void {
+    this.apiService.getCardInfo(1, 40,this.selectedClass).subscribe((data: any) => {
+      this.classCards = data.cards;
+      const seen = new Set();
+      this.cards = this.classCards.filter((card) => {
+        const name = card.name;
+        const duplicate = seen.has(name);
+        seen.add(name);
+        return !duplicate;
+      });
+    });
+  }
+
+  onSearch(event: Event) {
+    const searchTerm = (event.target as HTMLInputElement).value.toLowerCase();
+    this.filteredCards = this.cards.filter(card => card.name.toLowerCase().includes(searchTerm));
   }
 }
