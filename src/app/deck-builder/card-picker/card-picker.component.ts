844af6856e250f77e9c573c43174c5f983091462
Wrajt <kgrabarek59@gmail.com>
2024-07-24 16:39:18 +0200

    style improvements

--- src/app/deck-builder/card-picker/card-picker.component.ts
+++ src/app/deck-builder/card-picker/card-picker.component.ts
@@ -1,7 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {NgForOf, NgIf, TitleCasePipe} from "@angular/common";
-import {ApiService} from "../../api.service";
 import {PaginatorComponent} from "../../paginator/paginator.component";
+import {CardsDisplayComponent} from "../../cards-display/cards-display.component";
 
 @Component({
   selector: 'app-card-picker',
@@ -11,38 +11,23 @@ import {PaginatorComponent} from "../../paginator/paginator.component";
     NgForOf,
     TitleCasePipe,
     NgIf,
-    PaginatorComponent
+    PaginatorComponent,
+    CardsDisplayComponent
   ],
   styleUrls: ['./card-picker.component.css']
 })
-export class CardPickerComponent implements OnInit {
+export class CardPickerComponent {
   classNames = ['warrior', 'mage', 'druid', 'hunter', 'paladin', 'priest', 'rogue', 'shaman', 'warlock', 'neutral'];
   selectedClass: string = '';
-  classCards: any[] = [];
+
   cards: any[] = [];
   filteredCards: any[] = [];
 
-
-  constructor(private apiService: ApiService) {}
-
-  ngOnInit(): void {}
+  @ViewChild('cardsDisplay') cardsDisplay!: CardsDisplayComponent;
 
   onClassChange(event: Event): void {
     this.selectedClass = (event.target as HTMLSelectElement).value;
-    this.loadCards();
-  }
-
-  loadCards(): void {
-    this.apiService.getCardInfo(1, 40,this.selectedClass).subscribe((data: any) => {
-      this.classCards = data.cards;
-      const seen = new Set();
-      this.cards = this.classCards.filter((card) => {
-        const name = card.name;
-        const duplicate = seen.has(name);
-        seen.add(name);
-        return !duplicate;
-      });
-    });
+    this.cardsDisplay.loadCardInfo(1, this.cardsDisplay.pageSize, this.selectedClass);
   }
 
   onSearch(event: Event) {
